92114a1ecc923786003ab7a72edd8796a22a2de5
Дарья <fievdogs@gmail.com>
2026-02-27 10:38:51 +0700

    Add files via upload

---
+++ lab02/lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.labelTimeStep = new System.Windows.Forms.Label();
+            this.labelSpaceStep = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.comboBoxTau = new System.Windows.Forms.ComboBox();
+            this.comboBoxH = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colTau = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCenterTemp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelParams = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTimeStep
+            // 
+            this.labelTimeStep.AutoSize = true;
+            this.labelTimeStep.Location = new System.Drawing.Point(602, 356);
+            this.labelTimeStep.Name = "labelTimeStep";
+            this.labelTimeStep.Size = new System.Drawing.Size(119, 13);
+            this.labelTimeStep.TabIndex = 0;
+            this.labelTimeStep.Text = "Шаг по времени (τ), с:";
+            // 
+            // labelSpaceStep
+            // 
+            this.labelSpaceStep.AutoSize = true;
+            this.labelSpaceStep.Location = new System.Drawing.Point(602, 414);
+            this.labelSpaceStep.Name = "labelSpaceStep";
+            this.labelSpaceStep.Size = new System.Drawing.Size(115, 13);
+            this.labelSpaceStep.TabIndex = 1;
+            this.labelSpaceStep.Text = "Шаг по простр. (h), м:";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(618, 473);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 25);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Запуск";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(618, 502);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить таблицу";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // comboBoxTau
+            // 
+            this.comboBoxTau.FormattingEnabled = true;
+            this.comboBoxTau.Items.AddRange(new object[] {
+            "0,1",
+            "0,01",
+            "0,001",
+            "0,0001"});
+            this.comboBoxTau.Location = new System.Drawing.Point(600, 372);
+            this.comboBoxTau.Name = "comboBoxTau";
+            this.comboBoxTau.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTau.TabIndex = 4;
+            // 
+            // comboBoxH
+            // 
+            this.comboBoxH.FormattingEnabled = true;
+            this.comboBoxH.Items.AddRange(new object[] {
+            "0,1",
+            "0,01",
+            "0,001",
+            "0,0001"});
+            this.comboBoxH.Location = new System.Drawing.Point(600, 430);
+            this.comboBoxH.Name = "comboBoxH";
+            this.comboBoxH.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxH.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colTau,
+            this.colH,
+            this.colCenterTemp});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(445, 519);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // colTau
+            // 
+            this.colTau.HeaderText = "Шаг τ, с";
+            this.colTau.Name = "colTau";
+            // 
+            // colH
+            // 
+            this.colH.HeaderText = "Шаг h, м";
+            this.colH.Name = "colH";
+            // 
+            // colCenterTemp
+            // 
+            this.colCenterTemp.HeaderText = "T в центре, °C";
+            this.colCenterTemp.Name = "colCenterTemp";
+            this.colCenterTemp.Width = 200;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(463, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(574, 338);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelParams);
+            this.groupBox1.Location = new System.Drawing.Point(752, 356);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(153, 181);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // labelParams
+            // 
+            this.labelParams.AutoSize = true;
+            this.labelParams.Location = new System.Drawing.Point(6, 16);
+            this.labelParams.Name = "labelParams";
+            this.labelParams.Size = new System.Drawing.Size(82, 13);
+            this.labelParams.TabIndex = 0;
+            this.labelParams.Text = "Ждем запуска";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 542);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxH);
+            this.Controls.Add(this.comboBoxTau);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.labelSpaceStep);
+            this.Controls.Add(this.labelTimeStep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTimeStep;
+        private System.Windows.Forms.Label labelSpaceStep;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox comboBoxTau;
+        private System.Windows.Forms.ComboBox comboBoxH;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelParams;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTau;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCenterTemp;
+    }
+}
+
---
+++ lab02/lab2/lab2/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+
+        const double rho = 8900.0;   
+        const double c = 385.0;      
+        const double lambda = 400.0; 
+
+        const double L = 0.1;        
+        const double T_left = 100.0; 
+        const double T_right = 0.0;  
+        const double T_initial = 20.0; 
+        const double t_model = 9.0;  
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.ChartAreas.Clear();
+            var area = new ChartArea("MainArea");
+            area.AxisX.Title = "Координата x, м";
+            area.AxisY.Title = "Температура T, °C";
+            area.AxisX.Minimum = 0;
+            area.AxisX.Maximum = L;
+            chart1.ChartAreas.Add(area);
+
+            chart1.Series.Clear();
+            var series = new Series("TemperatureProfile")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = System.Drawing.Color.Blue,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series);
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(comboBoxTau.Text, out double tau))
+            {
+                MessageBox.Show("Ошибка parsing шага по времени", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!double.TryParse(comboBoxH.Text, out double h))
+            {
+                MessageBox.Show("Ошибка parsing шага по пространству", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (h <= 0 || tau <= 0)
+            {
+                MessageBox.Show("Шаги должны быть больше нуля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            labelParams.Text =
+                $"Материал: медь\n" +
+                $"ρ = {rho}\n" +
+                $"c = {c}\n" +
+                $"λ = {lambda}\n" +
+                $"L = {L}\n" +
+                $"τ = {tau}\n" +
+                $"h = {h}\n" +
+                $"T_left = {T_left}\n" +
+                $"T_right = {T_right}\n" +
+                $"T_initial = {T_initial}\n" +
+                $"t_model = {t_model}";
+
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                double tCenter = SolveHeatEquation(tau, h, out double[] T_final, out double[] xCoords);
+                sw.Stop();
+
+                labelParams.Text += $"\nВремя расчета: {sw.Elapsed.TotalMilliseconds:F2} мс";
+
+                int rowIndex = dataGridView1.Rows.Add();
+                dataGridView1["colTau", rowIndex].Value = tau;
+                dataGridView1["colH", rowIndex].Value = h;
+                dataGridView1["colCenterTemp", rowIndex].Value = tCenter.ToString("F4"); // 4 знака после запятой
+
+                DrawGraph(xCoords, T_final);
+
+                MessageBox.Show($"Расчет завершен!\nТемпература в центре: {tCenter:F4} °C", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при расчете: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private double SolveHeatEquation(double tau, double h, out double[] T_final, out double[] xCoords)
+        {
+            int N = (int)Math.Round(L / h);
+            if (N == 0) N = 1;
+            h = L / N;
+
+            int nodesCount = N + 1;
+
+            xCoords = new double[nodesCount];
+            double[] T = new double[nodesCount];
+
+            for (int i = 0; i < nodesCount; i++)
+            {
+                xCoords[i] = i * h;
+                T[i] = T_initial;
+            }
+
+            T[0] = T_left;
+            T[N] = T_right;
+
+            double coeff_A_C = lambda / (h * h);
+            double coeff_B_base = 2.0 * lambda / (h * h);
+
+            int timeSteps = (int)Math.Round(t_model / tau);
+            if (timeSteps == 0) timeSteps = 1;
+            tau = t_model / timeSteps;
+
+            double coeff_F_factor = rho * c / tau;
+
+            double[] alpha = new double[nodesCount];
+            double[] beta = new double[nodesCount];
+
+            for (int step = 0; step < timeSteps; step++)
+            {
+                alpha[0] = 0.0;
+                beta[0] = T_left;
+
+                for (int i = 1; i < N; i++)
+                {
+                    double B_i = coeff_B_base + coeff_F_factor;
+                    double denominator = B_i - coeff_A_C * alpha[i - 1];
+                    
+                    alpha[i] = coeff_A_C / denominator;
+                    beta[i] = (coeff_A_C * beta[i - 1] + coeff_F_factor * T[i]) / denominator;
+                }
+
+                T[N] = T_right;
+                for (int i = N - 1; i >= 1; i--)
+                {
+                    T[i] = alpha[i] * T[i + 1] + beta[i];
+                }
+                T[0] = T_left;
+            }
+            T_final = T;
+
+            int centerIndex = N / 2; 
+            return T_final[centerIndex];
+        }
+        private void DrawGraph(double[] x, double[] T)
+        {
+            chart1.Series[0].Points.Clear();
+            for (int i = 0; i < x.Length; i++)
+            {
+                chart1.Series[0].Points.AddXY(x[i], T[i]);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            chart1.Series[0].Points.Clear();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
