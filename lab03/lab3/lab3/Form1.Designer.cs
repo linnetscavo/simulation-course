ea4786171236c94f87a5360ce2b1d458ab3ef330
Дарья <fievdogs@gmail.com>
2026-03-12 12:29:46 +0700

    Add files via upload

---
+++ lab03/lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBoxMap = new PictureBox();
+            btnStart = new Button();
+            btnPause = new Button();
+            btnStop = new Button();
+            btnClear = new Button();
+            btnGenerate = new Button();
+            comboBoxSpeed = new ComboBox();
+            comboBoxWindDir = new ComboBox();
+            trackHumidity = new TrackBar();
+            trackWindForce = new TrackBar();
+            trackGrowth = new TrackBar();
+            trackLightning = new TrackBar();
+            timerSim = new System.Windows.Forms.Timer(components);
+            lblStep = new Label();
+            lblBurned = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMap).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackHumidity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackWindForce).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackGrowth).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackLightning).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxMap
+            // 
+            pictureBoxMap.Location = new Point(8, 12);
+            pictureBoxMap.Name = "pictureBoxMap";
+            pictureBoxMap.Size = new Size(500, 500);
+            pictureBoxMap.TabIndex = 0;
+            pictureBoxMap.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(621, 391);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(107, 48);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Старт";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnPause
+            // 
+            btnPause.Location = new Point(621, 460);
+            btnPause.Name = "btnPause";
+            btnPause.Size = new Size(105, 48);
+            btnPause.TabIndex = 2;
+            btnPause.Text = "Пауза";
+            btnPause.UseVisualStyleBackColor = true;
+            btnPause.Click += btnPause_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(762, 391);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(105, 48);
+            btnStop.TabIndex = 3;
+            btnStop.Text = "Стоп";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(762, 460);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(105, 48);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "Очистить";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(633, 327);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(220, 46);
+            btnGenerate.TabIndex = 5;
+            btnGenerate.Text = "Сгенерировать лес";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // comboBoxSpeed
+            // 
+            comboBoxSpeed.FormattingEnabled = true;
+            comboBoxSpeed.Items.AddRange(new object[] { "1", "2", "3", "4", "5" });
+            comboBoxSpeed.Location = new Point(551, 243);
+            comboBoxSpeed.Name = "comboBoxSpeed";
+            comboBoxSpeed.Size = new Size(121, 23);
+            comboBoxSpeed.TabIndex = 6;
+            comboBoxSpeed.Text = "3";
+            comboBoxSpeed.SelectedIndexChanged += comboBoxSpeed_SelectedIndexChanged;
+            // 
+            // comboBoxWindDir
+            // 
+            comboBoxWindDir.FormattingEnabled = true;
+            comboBoxWindDir.Items.AddRange(new object[] { "Север", "Юг", "Запад", "Восток" });
+            comboBoxWindDir.Location = new Point(798, 243);
+            comboBoxWindDir.Name = "comboBoxWindDir";
+            comboBoxWindDir.Size = new Size(121, 23);
+            comboBoxWindDir.TabIndex = 7;
+            comboBoxWindDir.Text = "Север";
+            comboBoxWindDir.SelectedIndexChanged += comboBoxWindDir_SelectedIndexChanged;
+            // 
+            // trackHumidity
+            // 
+            trackHumidity.LargeChange = 20;
+            trackHumidity.Location = new Point(768, 170);
+            trackHumidity.Maximum = 100;
+            trackHumidity.Name = "trackHumidity";
+            trackHumidity.Size = new Size(199, 45);
+            trackHumidity.TabIndex = 8;
+            trackHumidity.Scroll += trackHumidity_Scroll;
+            // 
+            // trackWindForce
+            // 
+            trackWindForce.LargeChange = 30;
+            trackWindForce.Location = new Point(768, 87);
+            trackWindForce.Maximum = 100;
+            trackWindForce.Name = "trackWindForce";
+            trackWindForce.Size = new Size(200, 45);
+            trackWindForce.TabIndex = 9;
+            trackWindForce.Scroll += trackWindForce_Scroll;
+            // 
+            // trackGrowth
+            // 
+            trackGrowth.Location = new Point(514, 170);
+            trackGrowth.Maximum = 100;
+            trackGrowth.Name = "trackGrowth";
+            trackGrowth.Size = new Size(200, 45);
+            trackGrowth.TabIndex = 10;
+            trackGrowth.Scroll += trackGrowth_Scroll;
+            // 
+            // trackLightning
+            // 
+            trackLightning.Location = new Point(514, 87);
+            trackLightning.Maximum = 100;
+            trackLightning.Name = "trackLightning";
+            trackLightning.Size = new Size(200, 45);
+            trackLightning.TabIndex = 11;
+            trackLightning.Scroll += trackLightning_Scroll;
+            // 
+            // timerSim
+            // 
+            timerSim.Tick += timerSim_Tick;
+            // 
+            // lblStep
+            // 
+            lblStep.AutoSize = true;
+            lblStep.Location = new Point(514, 27);
+            lblStep.Name = "lblStep";
+            lblStep.Size = new Size(41, 15);
+            lblStep.TabIndex = 12;
+            lblStep.Text = "Шаг: 0";
+            lblStep.Click += label1_Click;
+            // 
+            // lblBurned
+            // 
+            lblBurned.AutoSize = true;
+            lblBurned.Location = new Point(514, 12);
+            lblBurned.Name = "lblBurned";
+            lblBurned.Size = new Size(76, 15);
+            lblBurned.TabIndex = 13;
+            lblBurned.Text = "Сгорело: 0%";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(544, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 15);
+            label1.TabIndex = 14;
+            label1.Text = "Вероятность молнии (f)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(544, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 15);
+            label2.TabIndex = 15;
+            label2.Text = "Вероятность роста (p)";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(798, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 16;
+            label3.Text = "Сила ветра";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(798, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 15);
+            label4.TabIndex = 17;
+            label4.Text = "Влажность";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(551, 225);
+            label5.Name = "label5";
+            label5.Size = new Size(118, 15);
+            label5.TabIndex = 18;
+            label5.Text = "Скорость анимации";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(798, 225);
+            label6.Name = "label6";
+            label6.Size = new Size(114, 15);
+            label6.TabIndex = 19;
+            label6.Text = "Направление ветра";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(972, 524);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblBurned);
+            Controls.Add(lblStep);
+            Controls.Add(trackLightning);
+            Controls.Add(trackGrowth);
+            Controls.Add(trackWindForce);
+            Controls.Add(trackHumidity);
+            Controls.Add(comboBoxWindDir);
+            Controls.Add(comboBoxSpeed);
+            Controls.Add(btnGenerate);
+            Controls.Add(btnClear);
+            Controls.Add(btnStop);
+            Controls.Add(btnPause);
+            Controls.Add(btnStart);
+            Controls.Add(pictureBoxMap);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMap).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackHumidity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackWindForce).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackGrowth).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackLightning).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxMap;
+        private Button btnStart;
+        private Button btnPause;
+        private Button btnStop;
+        private Button btnClear;
+        private Button btnGenerate;
+        private ComboBox comboBoxSpeed;
+        private ComboBox comboBoxWindDir;
+        private TrackBar trackHumidity;
+        private TrackBar trackWindForce;
+        private TrackBar trackGrowth;
+        private TrackBar trackLightning;
+        private System.Windows.Forms.Timer timerSim;
+        private Label lblStep;
+        private Label lblBurned;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ lab03/lab3/lab3/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        bool isStopped = false;
+        const int GRID_SIZE = 50; 
+        const int CELL_SIZE = 10; 
+
+        enum CellState { Empty, Tree, Fire, Water }
+
+        CellState[,] grid = new CellState[GRID_SIZE, GRID_SIZE];
+        CellState[,] nextGrid = new CellState[GRID_SIZE, GRID_SIZE];
+
+        int currentStep = 0;
+        int totalTrees = 0;
+        int burnedTrees = 0;
+        bool isRunning = false;
+
+        Color colorEmpty = Color.White;
+        Color colorTree = Color.Green;
+        Color colorFire = Color.Orange;
+        Color colorWater = Color.LightBlue;
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMap();
+        }
+        private void InitializeMap()
+        {
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                for (int j = 0; j < GRID_SIZE; j++)
+                {
+                    grid[i, j] = CellState.Empty;
+                }
+            }
+            currentStep = 0;
+            burnedTrees = 0;
+            totalTrees = 0;
+            UpdateStats();
+            DrawMap();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            isStopped = false;
+            if (isRunning)
+            {
+                MessageBox.Show("Остановите симуляцию перед генерацией нового леса!", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            InitializeMap();
+            totalTrees = 0;
+
+            // 1. Генерируем лес (70% деревьев)
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                for (int j = 0; j < GRID_SIZE; j++)
+                {
+                    if (rand.NextDouble() < 0.7)
+                    {
+                        grid[i, j] = CellState.Tree;
+                        totalTrees++;
+                    }
+                }
+            }
+
+            // 2. Генерируем реку (горизонтальная полоса в случайном месте)
+            int riverRow = rand.Next(GRID_SIZE / 4, 3 * GRID_SIZE / 4);
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                if (grid[riverRow, j] == CellState.Tree)
+                    totalTrees--; // Река уничтожила дерево
+                grid[riverRow, j] = CellState.Water;
+
+                // Иногда добавляем второй ряд для ширины
+                if (rand.NextDouble() < 0.5 && riverRow + 1 < GRID_SIZE)
+                {
+                    if (grid[riverRow + 1, j] == CellState.Tree)
+                        totalTrees--;
+                    grid[riverRow + 1, j] = CellState.Water;
+                }
+            }
+
+            // 3. Поджигаем несколько случайных деревьев для старта
+            int fireCount = 5;
+            while (fireCount > 0)
+            {
+                int fi = rand.Next(GRID_SIZE);
+                int fj = rand.Next(GRID_SIZE);
+                if (grid[fi, fj] == CellState.Tree)
+                {
+                    grid[fi, fj] = CellState.Fire;
+                    fireCount--;
+                }
+            }
+
+            burnedTrees = 0;
+            currentStep = 0;
+            UpdateStats();
+            DrawMap();
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isStopped)
+            {
+                MessageBox.Show("Симуляция завершена. Сгенерируйте новый лес для продолжения.",
+                               "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (btnPause.Text == "Продолжить")
+            {
+                return; 
+            }
+
+            if (!isRunning)
+            {
+                isRunning = true;
+                timerSim.Start();
+                btnStart.Enabled = false;
+                btnStart.Text = "Идет...";
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                isRunning = false;
+                timerSim.Stop();
+                btnPause.Text = "Продолжить"; 
+            }
+            else
+            {
+                isRunning = true;
+                timerSim.Start();
+                btnPause.Text = "Пауза";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            isStopped = true;
+            timerSim.Stop();
+            btnPause.Text = "Пауза";
+            btnPause.Enabled = false;
+            MessageBox.Show($"Симуляция завершена!\n{lblBurned.Text}\nВсего шагов: {currentStep}",
+                    "Симуляция остановлена",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            timerSim.Stop();
+            InitializeMap();
+            btnPause.Enabled = true;
+            btnStart.Enabled = true;
+            btnStart.Text = "Старт";
+        }
+
+        private void timerSim_Tick(object sender, EventArgs e)
+        {
+            SimulateStep();
+            currentStep++;
+            UpdateStats();
+            DrawMap();
+        }
+
+        private void SimulateStep()
+        {
+            Array.Copy(grid, nextGrid, grid.Length);
+
+            double f = trackLightning.Value / 1000.0; 
+            double p = trackGrowth.Value / 100.0;     
+            double humidity = trackHumidity.Value / 100.0; 
+            double windForce = trackWindForce.Value / 100.0; 
+
+            int windX = 0, windY = 0;
+            string windDir = comboBoxWindDir.Text;
+            if (windDir == "Север") windY = 1; 
+            else if (windDir == "Юг") windY = -1; 
+            else if (windDir == "Запад") windX = 1; 
+            else if (windDir == "Восток") windX = -1;
+
+            int newBurned = 0;
+
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                for (int j = 0; j < GRID_SIZE; j++)
+                {
+                    CellState current = grid[i, j];
+
+                    if (current == CellState.Fire)
+                    {
+                        nextGrid[i, j] = CellState.Empty;
+                        newBurned++;
+                    }
+                    else if (current == CellState.Tree)
+                    {
+                        bool hasBurningNeighbor = false;
+                        double maxFireProb = 0.0;
+
+                        for (int di = -1; di <= 1; di++)
+                        {
+                            for (int dj = -1; dj <= 1; dj++)
+                            {
+                                if (di == 0 && dj == 0) continue;
+
+                                int ni = i + di;
+                                int nj = j + dj;
+
+                                if (ni >= 0 && ni < GRID_SIZE && nj >= 0 && nj < GRID_SIZE)
+                                {
+                                    if (grid[ni, nj] == CellState.Fire)
+                                    {
+                                        hasBurningNeighbor = true;
+
+                                        double windFactor = (dj * windX + di * windY) * windForce;
+                                        double fireProb = 0.85 + windFactor; 
+
+                                        fireProb *= (1.0 - humidity * 0.5);
+
+                                        if (fireProb > maxFireProb)
+                                            maxFireProb = fireProb;
+                                    }
+                                }
+                            }
+                        }
+                        if (hasBurningNeighbor && rand.NextDouble() < maxFireProb)
+                        {
+                            nextGrid[i, j] = CellState.Fire;
+                        }
+                        else if (rand.NextDouble() < f * (1.0 - humidity))
+                        {
+                            nextGrid[i, j] = CellState.Fire;
+                        }
+                    }
+                    else if (current == CellState.Empty)
+                    {
+                        if (rand.NextDouble() < p * (1.0 - humidity * 0.3))
+                        {
+                            nextGrid[i, j] = CellState.Tree;
+                            totalTrees++;
+                        }
+                    }
+                }
+            }
+
+            burnedTrees += newBurned;
+            Array.Copy(nextGrid, grid, grid.Length);
+        }
+
+        private void DrawMap()
+        {
+            Bitmap bmp = new Bitmap(GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    for (int j = 0; j < GRID_SIZE; j++)
+                    {
+                        Color cellColor = colorEmpty;
+                        switch (grid[i, j])
+                        {
+                            case CellState.Tree: cellColor = colorTree; break;
+                            case CellState.Fire: cellColor = colorFire; break;
+                            case CellState.Water: cellColor = colorWater; break;
+                        }
+
+                        using (SolidBrush brush = new SolidBrush(cellColor))
+                        {
+                            g.FillRectangle(brush, j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                        }
+                    }
+                }
+            }
+            pictureBoxMap.Image = bmp;
+        }
+
+        private void UpdateStats()
+        {
+            lblStep.Text = $"Шаг: {currentStep}";
+
+            double burnedPercent = totalTrees > 0 ? (burnedTrees * 100.0 / totalTrees) : 0;
+            lblBurned.Text = $"Сгорело: {burnedPercent:F1}%";
+        }
+
+        private void comboBoxSpeed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int speed = int.Parse(comboBoxSpeed.Text);
+            timerSim.Interval = 550 - (speed * 100);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackLightning_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackGrowth_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackWindForce_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackHumidity_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxWindDir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
